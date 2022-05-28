5f5b786429057d22862dc049b6d5ca20da2b559c
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-28 16:55:09 -0700

    Added partail ConEmu

---
+++ Programs/ConEmu.ps1
@@ -0,0 +1,11 @@
+# ConEmu has it's only special command line installation method that allows more flexibility than Winget or Chocolatey
+param ($force)
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
+
+# set lnk $TRUE; is supposed to make it where no desktop shortcut is made
+#   Instead it creates a shortcut called False.lnk
+# TODO: Copy configuration file
+powershell -NoProfile -ExecutionPolicy RemoteSigned -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; set dst 'C:\Programs\ConEmu'; set lnk $FALSE; iex ((new-object net.webclient).DownloadString('https://conemu.github.io/install2.ps1'))"
+
+# Because the installer is a little bit broken the shortcut needs to be deleted if it exists
+delete_shortcut "False"
