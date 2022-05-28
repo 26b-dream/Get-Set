a68cc3e74e6e5115ea0114fc74152942649f4412
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-28 16:40:35 -0700

    Added RME Fireface UFX+

---
+++ Drivers/RME Fireface UFX+.ps1
@@ -0,0 +1,19 @@
+# Not available on Winget
+# Not available on Chocolatey
+param ($force)
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
+
+$version = "09747"
+
+$download_folder = "$env:USERPROFILE/Downloads/get_set/Drivers/RME Fireface UFX+"
+$url = "https://rme-audio.de/downloads/driver_madiface_win_$version.zip"
+$zip_path="$download_folder/driver_madiface_win_$version.zip"
+$extracted_path="$download_folder/driver_madiface_win_$version"
+
+download $url $zip_path
+extract $zip_path $extracted_path
+. "$extracted_path/rmeinstaller.exe" /silent
+
+Write-Host "Activate devices in madiface" -ForegroundColor DarkYellow
+Write-Host "Set ADAT 1/2 and ADAT 3/4 as loopback" -ForegroundColor DarkYellow
+Write-Host "Disable Windows audio devices" -ForegroundColor DarkYellow
