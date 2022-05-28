31b5a4c9d0cce006b952fc8533dad913d979fb2e
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-28 16:52:03 -0700

    Added Everything

---
+++ Programs/Everything.ps1
@@ -0,0 +1,8 @@
+param ($force)
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
+
+winget_install_v2 $PSCommandPath "voidtools.Everything" -force $force
+delete_shortcut $PSCommandPath
+
+# Everything does not start automatically after installation so it is safe to restore config after installation
+Copy-Item files\Everything\Everything.ini C:\Programs\$name\Everything.ini
