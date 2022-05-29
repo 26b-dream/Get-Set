35e5a6523e665d577068913a6a0ea877ade16e14
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-28 18:05:10 -0700

    Added ffmpeg

---
+++ Programs/ffmpeg.ps1
@@ -0,0 +1,29 @@
+# Not available on Winget
+param ($force)
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
+
+$type = "Programs"
+$name = "ffmpeg"
+$package = "$name"
+$install_path = (install_path $PSCommandPath)
+
+if ( !(Test-Path -Path $install_path) -or $force) {
+    # Zip installer
+    #   Extracts to C:\ProgramData\chocolatey\lib\ffmpeg\tools\ffmpeg
+    choco_install $PSCommandPath -force $force
+
+    # Delete old version if it exists
+    if (Test-Path -Path $install_path) {
+        Remove-Item $install_path -Recurse
+    }
+    
+    # Copy files into proper install location
+    Copy-Item "C:\ProgramData\chocolatey\lib\ffmpeg\tools\ffmpeg" $install_path -Recurse
+    
+    # Uninstall because chocolatey's files are no longer needed
+    choco uninstall $package -y
+    
+    add_to_path "$install_path\bin\ffmpeg.exe"
+
+    Write-Host "$name installed" -ForegroundColor Green
+}
