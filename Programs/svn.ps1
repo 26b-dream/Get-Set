77d74b514bd57cd0c3d1b5d44aca1cac66d0dee9
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-29 05:00:18 -0700

    Added svn

---
+++ Programs/svn.ps1
@@ -0,0 +1,7 @@
+# Not available on Winget
+param ($force)
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
+
+$params = "InstallDir=$(install_path $PSCommandPath)"
+
+choco_install $PSCommandPath -params $params -force $force
