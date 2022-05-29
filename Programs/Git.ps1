178ab93e33e9dfcfd6a14edb18bd057872ebf0ba
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-28 17:06:48 -0700

    Added Git

---
+++ Programs/Git.ps1
@@ -0,0 +1,13 @@
+param ($force)
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
+
+# Does not set default editor correctly
+#   winget install -e --id Git.Git --override "/EditorOption=VisualStudioCode"
+# --override to customize installation
+#   /LOADINF=files\Git\Git.inf
+#       Load custom install settings
+#       See: files\Git\Git.inf
+#   /SILENT - install silently 
+$params = "/LOADINF=files\$name\install.inf /SILENT"
+
+winget_install_v2 $PSCommandPath -params $params -force $force
