c844843398dfcd478cbd9b1b6c2958debc8ecbef
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-28 17:09:07 -0700

    Fixed GitHub Desktop

--- Programs/GitHub Desktop.ps1
+++ Programs/GitHub Desktop.ps1
@@ -1,10 +1,7 @@
 # Squirrel Installer
 #   Impossible to modify installation path, see: https://github.com/desktop/desktop/issues/1086
-
 param ($force)
 . "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
 
-winget_install "Programs" "GitHub Desktop" "GitHub.GitHubDesktop" -force $force
+winget_install_v2 "GitHub.GitHubDesktop" $PSCommandPath -force $force
 delete_shortcut "GitHub Desktop"
-
-Write-Host "Login to GitHub Desktop" -ForegroundColor DarkYellow
