e9b4cabae08bbdaf6d9f96db4d55533a08b52046
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-05-31 16:07:36 -0700

    Added yt-dlp

---
+++ Programs/yt-dlp.ps1
@@ -0,0 +1,28 @@
+# Not available on Winget
+param ($force)
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\load.ps1"
+
+$name = "yt-dlp"
+$package = "$name"
+$install_path = (install_path $PSCommandPath)
+
+if ( !(Test-Path -Path $install_path) -or $force) {
+    # Zip installer
+    #   Extracts to C:\ProgramData\chocolatey\lib\ffmpeg\tools\ffmpeg
+    choco_install $PSCommandPath -force $force
+
+    # Delete old version if it exists
+    if (Test-Path -Path $install_path) {
+        Remove-Item $install_path -Recurse
+    }
+    
+    # Copy files into proper install location
+    Copy-Item "C:\ProgramData\chocolatey\lib\yt-dlp" $install_path -Recurse
+    
+    # # Uninstall because chocolatey's files are no longer needed
+    choco uninstall $package -y
+    
+    add_to_path "$install_path\tools\x64\yt-dlp.exe"
+
+    Write-Host "$name installed" -ForegroundColor Green
+}
